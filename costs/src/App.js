574a9c693e5f1b528d88f36032f05dfba9efbbad
italloalves99 <134318969+italloalves99@users.noreply.github.com>
2023-10-16 13:23:50 -0300

    estruturando o projeto

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,14 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
+import Container from './components/layout/Container';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <p>
-         costs
-        </p>
-        
-      </header>
+      <Router>
+        <div>
+          <Link to='/'>Home</Link>
+          <Link to='/contact'>Contato</Link>
+          <Link to='/company'>Empresa</Link>
+          <Link to='/newproject'>Novo Projeto</Link>
+        </div>
+        <Routes>
+          <Route path='/' element={<Container><Home /></Container>} />
+          <Route path='/company' element={<Container><Company /></Container>} />
+          <Route path='/contact' element={<Container><Contact /></Container>} />
+          <Route path='/newproject' element={<Container><NewProject /></Container>} />
+        </Routes>
+        <footer>Footer</footer>
+      </Router>
     </div>
   );
 }
---
+++ costs/src/components/pages/Home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Home() {
+  return <div>Conteúdo da Página Inicial</div>;
+}
+
+export default Home;
